3045d7147a7623b80c2c395b188d4c12afd11788
Chris Haynes <cghaynes@toastsideup.co.uk>
2020-11-12 13:12:17 +0000

    Add User ID to migration, before running.

--- database/migrations/2020_11_10_123729_create_feeds_table.php
+++ database/migrations/2020_11_10_123729_create_feeds_table.php
@@ -18,6 +18,7 @@ class CreateFeedsTable extends Migration
             $table->string('name');
             $table->string('url');
             $table->boolean('active');
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
         });
     }
